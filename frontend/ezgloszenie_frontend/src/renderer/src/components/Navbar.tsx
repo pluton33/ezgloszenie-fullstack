3a908bd172b35581a6759f5c83ef56f24e555ad3
pyrciu1 <kacperp4123@gmail.com>
2026-04-22 14:40:49 +0200

    navbar update

--- frontend/ezgloszenie_frontend/src/renderer/src/components/Navbar.tsx
+++ frontend/ezgloszenie_frontend/src/renderer/src/components/Navbar.tsx
@@ -5,11 +5,14 @@ import { LightbulbIcon } from 'lucide-react';
 function Navbar(): React.JSX.Element {
     return (
         <div className='topbar'>
+            {/* Logo policji */}
             <img src={logoUrl} className='topbar-logo' alt="Logo" />
-            <LightbulbIcon className='zaruwka' />
+            {/* Napis ezgloszenie */}
+            <span className="nazwa">eZgłoszenie</span>
+            {/* Przycisk i ikona zaurwki */}
+            <button className="buttonzaruwka"><LightbulbIcon className='zaruwka' /></button>
         </div>
     )
-
 }
 
 export default Navbar
