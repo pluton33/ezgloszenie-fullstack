2ee3654bab571af05f6dcbf6f329a9154fdaa2e0
igiu7 <igigamer7@gmail.com>
2026-04-22 16:38:57 +0200

    added light and dark theme, font for logo and a smooth animation to it

--- frontend/ezgloszenie_frontend/src/renderer/src/components/Navbar.tsx
+++ frontend/ezgloszenie_frontend/src/renderer/src/components/Navbar.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import logoUrl from '../assets/policja-logo.svg';
-import { LightbulbIcon } from 'lucide-react';
+import '../assets/base.css';
+import React from 'react'
+import { useState } from 'react';
+import logoUrl from '../assets/policja-logo.svg'
+import { LightbulbIcon, LightbulbOffIcon } from 'lucide-react'
 
 function Navbar(): React.JSX.Element {
+    const [isDark, setIsDark] = useState(false);
+
+    const toggleTheme = (): void => {
+        if (isDark) {
+            document.documentElement.removeAttribute('data-theme');
+            setIsDark(false);
+
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            setIsDark(true);
+
+        }
+    };
+
     return (
         <div className='topbar'>
             {/* Logo policji */}
             <img src={logoUrl} className='topbar-logo' alt="Logo" />
             {/* Napis ezgloszenie */}
-            <span className="nazwa">eZgłoszenie</span>
+            <span className="nazwa">e-Zgłoszenie</span>
             {/* Przycisk i ikona zaurwki */}
-            <button className="buttonzaruwka"><LightbulbIcon className='zaruwka' /></button>
+            <button onClick={toggleTheme} className="buttonzaruwka">
+                {isDark ? (<LightbulbOffIcon className='zaruwka' />) : (<LightbulbIcon className='zaruwka' />)}
+            </button>
         </div>
     )
+
 }
 
 export default Navbar
