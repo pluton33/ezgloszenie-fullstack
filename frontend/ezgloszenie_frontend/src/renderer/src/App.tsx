4f52c30564d3b4e63fedd26854bcc676343024a0
igiu7 <igigamer7@gmail.com>
2026-04-23 11:23:55 +0200

    added a register component

--- frontend/ezgloszenie_frontend/src/renderer/src/App.tsx
+++ frontend/ezgloszenie_frontend/src/renderer/src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar'
-import Login from './components/Login'
+{/*import Login from './components/Login'*/ }
+import Register from './components/Register'
 
 function App(): React.JSX.Element {
 
   return (
     <>
       <Navbar />
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </>
   )
 }
