6c431125b4deb1ce487effa0b9432338cdda7a39
igiu7 <igigamer7@gmail.com>
2026-04-23 10:59:50 +0200

    added a register button and labels

--- frontend/ezgloszenie_frontend/src/renderer/src/components/Login.tsx
+++ frontend/ezgloszenie_frontend/src/renderer/src/components/Login.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react';
 import { LockIcon, MailIcon, SquareArrowRightEnter } from 'lucide-react';
 
 function Login(): React.JSX.Element {
@@ -8,19 +7,30 @@ function Login(): React.JSX.Element {
 
     return (
         <div className='login-content'>
-            <div className='login-text' style={{ marginTop: '15vw' }} >
+            <label className='login-text-label' htmlFor="username">Adres e-mail:</label>
+            <div className='login-text'>
                 <MailIcon className='login-icon' size={16} />
-                <input className='login-text-input' name="username" />
+                <input type='text' className='login-text-input' name="username" />
             </div>
-            <div className='login-text' style={{ marginTop: '20px' }}>
+            <label className='login-text-label' style={{ marginTop: '20px' }} htmlFor="password">Hasło:</label>
+            <div className='login-text' >
                 <LockIcon className='login-icon' size={16} />
-                <input className='login-text-input' name="password" />
+                <input type='password' className='login-text-input' name="password" />
             </div>
 
-
-
-            <button className='login-btn'>
+            <button className='login-btn' style={{ marginTop: '30px' }}>
                 <span className='login-btn-text'>Zaloguj Się</span>
+                <SquareArrowRightEnter className='key-icon' size={16} />
+            </button>
+
+            <div className='login-lub' style={{ marginTop: '18px' }}>
+                <hr className='login-hr' />
+                <span className='login-text-label' >lub</span>
+                <hr className='login-hr' />
+            </div>
+
+            <button className='login-btn' style={{ marginTop: '18px' }}>
+                <span className='login-btn-text'>Załóż konto</span>
                 <SquareArrowRightEnter className='key-icon' size={16} />
             </button>
 
