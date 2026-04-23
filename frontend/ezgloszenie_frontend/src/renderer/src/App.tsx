f0157de3cb28f743fc25a29da2d53a50981f6101
pyrciu1 <kacperp4123@gmail.com>
2026-04-23 13:36:38 +0200

    added quickmenu (home page content buttons) added profile icon in navbar

--- frontend/ezgloszenie_frontend/src/renderer/src/App.tsx
+++ frontend/ezgloszenie_frontend/src/renderer/src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App(): React.JSX.Element {
 
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
