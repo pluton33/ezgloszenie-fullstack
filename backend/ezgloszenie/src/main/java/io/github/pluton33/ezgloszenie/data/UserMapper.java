da80a363669c7346d8230fee7becad2e2bcd558a
Sebastian Puła <pulasebastian@gmail.com>
2026-04-28 15:33:37 +0200

    added map struct and user mapper

---
+++ backend/ezgloszenie/src/main/java/io/github/pluton33/ezgloszenie/data/UserMapper.java
@@ -0,0 +1,9 @@
+package io.github.pluton33.ezgloszenie.data;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toDto(UserEntity userEntity);
+    UserEntity toEntity(User userDTO);
+}
