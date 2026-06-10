b388ed0ad67c862f98307fb4b3bc1c4cb263353a
Sebastian Puła <pulasebastian@gmail.com>
2026-06-10 18:26:01 +0200

    add: new fields to report

--- backend/ezgloszenie/src/main/java/io/github/pluton33/ezgloszenie/data/Report.java
+++ backend/ezgloszenie/src/main/java/io/github/pluton33/ezgloszenie/data/Report.java
@@ -3,9 +3,18 @@ package io.github.pluton33.ezgloszenie.data;
 import java.time.LocalDateTime;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Null;
 
-public record Report(@Nullable Long id, String title, String description, @Nullable String status,@Nullable Category category,User user, LocalDateTime created_date) {
+public record Report(@Nullable Long id,
+                     String title,
+                     String description,
+                     @Nullable String status,
+                     @Nullable Category category,
+                     User user,
+                     LocalDateTime created_date,
+                     LocalDateTime accident_date,
+                     String location,
+                     boolean userAnonymous
+                     ) {
 //    public static Report fromEntity(ReportEntity entity) {
 //        return new Report(entity.getId(), entity.getTitle(), entity.getDescription(), entity.getUser());
 //    }
